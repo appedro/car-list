0f1514d355c5a9d7c78a20340567f7d67c583a53
pedro.oliveira <.>
2024-11-10 17:14:11 -0300

    feat: show price and license plate number

--- src/app/shared/car-card/car-card.component.ts
+++ src/app/shared/car-card/car-card.component.ts
@@ -1,15 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { Car } from '../models/car';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-car-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './car-card.component.html',
   styleUrl: './car-card.component.scss'
 })
 export class CarCardComponent {
   @Input() public car!: Car;
+
+  public getLastLicensePlate(licensePlate: string): string {
+    return licensePlate[licensePlate.length - 1];
+  }
 
   public getBrandLogo (brand: string): string {
     return `assets/images/brand/${brand.toLocaleLowerCase()}.png`
