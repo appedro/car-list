1ec54c02e0f29faa4c01e25006a1bdfbe39a0249
pedro.oliveira <.>
2024-11-10 17:01:18 -0300

    feat: display car list

--- src/app/pages/car-list-page/car-list-page.component.ts
+++ src/app/pages/car-list-page/car-list-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Car } from '../../shared/models/car';
 import { CarListPageService } from './car-list-page.service';
+import { CarCardComponent } from '../../shared';
 
 @Component({
   selector: 'app-car-list-page',
   standalone: true,
-  imports: [],
+  imports: [CarCardComponent],
   providers: [CarListPageService],
   templateUrl: './car-list-page.component.html',
   styleUrl: './car-list-page.component.scss'
--- src/app/shared/car-card/car-card.component.ts
+++ src/app/shared/car-card/car-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Car } from '../models/car';
 
 @Component({
   selector: 'app-car-card',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './car-card.component.scss'
 })
 export class CarCardComponent {
+  @Input() public car!: Car;
 
+  public getBrandLogo (brand: string): string {
+    return `assets/images/brand/${brand.toLocaleLowerCase()}.png`
+  }
 }
